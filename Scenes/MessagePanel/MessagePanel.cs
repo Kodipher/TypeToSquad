a96a1631c5d5bb11539357b697974fedf42c46f9
Kodi <45706822+Kodipher@users.noreply.github.com>
2023-08-05 01:44:55 +0300

    Added message input clearing on speak

--- Scenes/MessagePanel/MessagePanel.cs
+++ Scenes/MessagePanel/MessagePanel.cs
@@ -9,9 +9,17 @@ public partial class MessagePanel : Control {
 	
 	public override void _Ready() {
 
+		// Connect and expose speak button
 		MessageEdit messageEdit = GetNode<MessageEdit>("%MessageEdit");
 
-		GetNode<Button>("%ButtonSpeak").Pressed += delegate { EmitSignal(SignalName.SpeakPressed, messageEdit.Text); };
+		void OnSpeakPress() {
+			EmitSignal(SignalName.SpeakPressed, messageEdit.Text);
+			messageEdit.Clear();
+		}
+
+		GetNode<Button>("%ButtonSpeak").Pressed += OnSpeakPress;
+
+		// Connect and expose other buttons
 		GetNode<Button>("%ButtonShut").Pressed += delegate { EmitSignal(SignalName.ShutPressed); };
 		GetNode<Button>("%ButtonConfig").Pressed += delegate { EmitSignal(SignalName.ConfigPressed); };
 		GetNode<Button>("%ButtonInfo").Pressed += delegate { EmitSignal(SignalName.InfoPressed); }; ;
