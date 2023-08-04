dc4297586a53b8caed1d4dcc7d42e53bcbfb8bb2
Kodi <45706822+Kodipher@users.noreply.github.com>
2023-08-04 22:01:42 +0300

    Created message edit script

---
+++ Scenes/MessageInput/MessageEdit.cs
@@ -0,0 +1,18 @@
+using Godot;
+using System;
+
+
+namespace Scenes.MessageInput;
+
+
+public partial class MessageEdit : TextEdit {
+
+
+	public override void _Ready() {
+	}
+
+	public override void _Input(InputEvent @event) {
+		base._Input(@event);
+	}
+
+}
