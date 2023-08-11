54aed3957d6c0f83e09b7386da768dad22ccc011
Kodi <45706822+Kodipher@users.noreply.github.com>
2023-08-11 19:47:43 +0300

    Changed default for MaxConcurentStreams

--- Modules/Configuration/Configuration.cs
+++ Modules/Configuration/Configuration.cs
@@ -20,6 +20,6 @@ public class Configuration {
 	);
 
 	/// <summary>The max number of tts outputs that are played at the same time</summary>
-	public readonly FieldIntRange MaxConcurentStreams = new(1, 64, defaultValue: 1);
+	public readonly FieldIntRange MaxConcurentStreams = new(1, 64, defaultValue: 6);
 
 }
