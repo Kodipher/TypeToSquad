693c057b913095a334bd9b0eef1ccedb7c629fdc
Kodi <45706822+Kodipher@users.noreply.github.com>
2023-08-15 03:27:22 +0300

    Forced configuration to be init causes its shared

--- Modules/Configuration/ConfigurationManager.cs
+++ Modules/Configuration/ConfigurationManager.cs
@@ -13,9 +13,10 @@ public partial class ConfigurationManager : Node {
 	/// <summary>
 	/// Current configuration used by pretty much everything.
 	/// Any changed to the object are to be reflected on the spot.
+	/// Set only one time on init due to sharing by reference.
 	/// Saving the obejct to disk is separate. See <see cref="SaveCurrentConfig"/>
 	/// </summary>
-	public Configuration CurrentConfig { get; protected set; } = new Configuration();
+	public Configuration CurrentConfig { get; init; } = new Configuration();
 
 	#region //// Saving / Loading
 
