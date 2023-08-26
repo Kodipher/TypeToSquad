759a14fcd8333b1429ae15915eb665b3d096a4bb
Kodi <45706822+Kodipher@users.noreply.github.com>
2023-08-26 18:17:39 +0300

    Added active speech storage and disposal

---
+++ Modules/Speech/ActiveSpeech.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.IO;
+using System.Speech.Synthesis;
+using System.Speech.AudioFormat;
+using NAudio.CoreAudioApi;
+using NAudio.Wave;
+
+
+namespace Kodipher.TypeToSquad.Modules.Speech;
+
+
+/// <summary>
+/// A class that creates or takes ownership of
+/// references needed to synthesize and play speech
+/// </summary>
+[System.Runtime.Versioning.SupportedOSPlatform("windows")]
+public class ActiveSpeech : IDisposable {
+
+	#region //// Ownership
+
+	public MMDevice OutputDevice { get; private set; }
+	public WasapiOut OutputPlayer { get; private set; }
+	public SpeechSynthesizer Synthesizer { get; private set; }
+
+	[System.Runtime.Versioning.SupportedOSPlatform("windows")]
+	public ActiveSpeech(MMDevice device) {
+		OutputDevice = device;
+
+		OutputPlayer = new(OutputDevice, AudioClientShareMode.Shared, true, 200);
+
+		Synthesizer = new SpeechSynthesizer();
+		SpeechApiReflectionHelper.InjectOneCoreVoices(Synthesizer);
+	}
+
+	#endregion
+
+	#region //// Playing and stopping
+
+	public void Speak() {
+
+		// Link events
+		OutputPlayer.PlaybackStopped += (object? _sender, StoppedEventArgs _args) => { IsReadyForDisposal = true; };
+
+		// Link synthesier to output player
+		// todo
+
+		// Perfom speech
+		// todo
+
+	}
+
+	public void Shut() {
+		if (!readyForDisposal) OutputPlayer.Stop();
+	}
+
+	#endregion
+
+	#region //// Disposing
+
+	private bool readyForDisposal;
+	public bool IsReadyForDisposal {
+		get => readyForDisposal;
+		protected set {
+			readyForDisposal = value;
+			if (value) OnReadyForDisposal?.Invoke(this, EventArgs.Empty);
+		}
+	}
+
+	public event EventHandler? OnReadyForDisposal;
+
+
+	private bool hasDisposed;
+
+	protected virtual void Dispose(bool disposeManaged) {
+
+		if (hasDisposed) return;
+
+		if (disposeManaged) {
+			OutputDevice.Dispose();
+			OutputPlayer.Dispose();
+			Synthesizer.Dispose();
+		}
+
+		OutputDevice = null!;
+		OutputPlayer = null!;
+		Synthesizer = null!;
+
+		hasDisposed = true;
+	}
+
+	~ActiveSpeech() {
+		Dispose(disposeManaged: false);
+	}
+
+	public void Dispose() {
+		Dispose(disposeManaged: true);
+		GC.SuppressFinalize(this);
+	}
+
+	#endregion
+
+}
+
--- Modules/Speech/SpeechController.cs
+++ Modules/Speech/SpeechController.cs
@@ -55,6 +55,93 @@ public partial class SpeechController : Node {
 
 	#endregion
 
+	#region //// Active speech storage
+
+	//
+	// When ActiveSpeech objects are created they are placed in activeStreams
+	// When ActiveSpeech are forcefully shut they are put in shutStreams
+	// When ActiveSpeech are finished in either list they are diposed of
+	//
+
+	readonly LinkedList<ActiveSpeech> activeStreams = new();
+
+	readonly LinkedList<ActiveSpeech> shutStreams = new();
+
+	private void AddActiveSpeech(ActiveSpeech speech) {
+
+		// Ready guard
+		if (!IsNodeReady()) {
+			speech.Dispose();
+			throw new InvalidOperationException("Cannot add active speech: Node is not ready.");
+		}
+
+		// Double check completed
+		CheckSpeechesForDisposal();
+
+		// Remove the oldest speeches if going over the threshold
+		int maxConcurentStreams = configManager.CurrentConfig.MaxConcurrentStreams.Value;
+		if (activeStreams.Count >= maxConcurentStreams) {
+
+			int streamsToClear = activeStreams.Count + 1 - maxConcurentStreams;
+
+			for (int i = 0; i < streamsToClear; i++) {
+				ShutOldestSpeech();
+			}
+
+		}
+
+		// Add current stream at the end
+		activeStreams.AddLast(speech);
+	}
+
+	private void ShutOldestSpeech() {
+
+		// Find first in the list -- oldest;
+		ActiveSpeech? streamToEnd = activeStreams.First?.Value;
+		if (streamToEnd is null) return;
+
+		// If exists: shut
+		streamToEnd.Shut();
+		activeStreams.RemoveFirst();
+		shutStreams.AddLast(streamToEnd);
+	}
+
+	private void CheckSpeechesForDisposal() {
+
+		LinkedListNode<ActiveSpeech>? currentNode;
+
+		// Check every active speech if it is ready for disposal
+		currentNode = activeStreams.First;
+		while(currentNode is not null) {
+			if (currentNode.Value.IsReadyForDisposal) {
+				// If it is - dispose
+				LinkedListNode<ActiveSpeech>? nextNode = currentNode.Next;
+				activeStreams.Remove(currentNode);
+				currentNode.Value.Dispose();
+				currentNode = nextNode;
+			} else {
+				currentNode = currentNode.Next;
+			}
+		}
+
+		// Check every shut node if it is ready for disposal
+		currentNode = shutStreams.First;
+		while (currentNode is not null) {
+			if (currentNode.Value.IsReadyForDisposal) {
+				// If it is - dispose
+				LinkedListNode<ActiveSpeech>? nextNode = currentNode.Next;
+				activeStreams.Remove(currentNode);
+				currentNode.Value.Dispose();
+				currentNode = nextNode;
+			} else {
+				currentNode = currentNode.Next;
+			}
+		}
+
+	}
+
+	#endregion
+
 	#region //// Perform synthesis
 
 	/// <summary>
@@ -66,7 +153,36 @@ public partial class SpeechController : Node {
 		// System guard
 		if (!OperatingSystem.IsOSPlatform("windows")) throw new PlatformNotSupportedException();
 
+		// Ready guard
+		if (!IsNodeReady()) {
+			throw new InvalidOperationException("Cannot perform syntehsis: Node is not ready.");
+		}
+
 		GD.Print($"Speaking \"{text}\"");
+
+		// Setup active speech
+		MMDevice device;
+		using (MMDeviceEnumerator enumerator = new()) {
+
+			string deviceID = configManager.CurrentConfig.Device.DeviceID;
+
+			if (deviceID == defaultDevice.ID) {
+				device = enumerator.GetDefaultAudioEndpoint(DataFlow.Render, Role.Console);
+			} else {
+				device = enumerator.GetDevice(deviceID);
+			}
+		}
+
+		ActiveSpeech speech = new(device);
+
+		// Play
+		// todo
+		//outputDevice.Init(audioFile);
+		//outputDevice.Play();
+
+		// Add active speech to storage
+		AddActiveSpeech(speech);
+		speech.OnReadyForDisposal += (object? _sender, EventArgs _) => CheckSpeechesForDisposal();
 
 	}
 
@@ -75,6 +191,12 @@ public partial class SpeechController : Node {
 	/// </summary>
 	public void Shut() {
 		GD.Print("Shutting up");
+
+		while (activeStreams.Count > 0) {
+			ShutOldestSpeech();
+		}
+
+		CheckSpeechesForDisposal();
 	}
 
 	#endregion
