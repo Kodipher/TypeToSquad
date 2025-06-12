2f38c488fb9658a3e221576d58b20e52f3aab8e5
Kodipher <45706822+Kodipher@users.noreply.github.com>
2025-06-12 23:50:44 +0200

    Create test main scene

---
+++ src/Scenes/MainScene.cs
@@ -0,0 +1,17 @@
+using Godot;
+using System;
+
+
+namespace TypeToSquad.Scenes;
+
+
+public partial class MainScene : HBoxContainer {
+
+	public override void _Ready() {
+	}
+
+
+	public override void _Process(double delta) {
+	}
+
+}
