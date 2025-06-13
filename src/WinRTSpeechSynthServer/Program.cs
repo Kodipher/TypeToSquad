fe4f2c46c4dae323a54878956a04d7072859e47b
Kodipher <45706822+Kodipher@users.noreply.github.com>
2025-06-13 16:17:07 +0200

    Create speech synth server solution

---
+++ src/WinRTSpeechSynthServer/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Windows.Media.SpeechSynthesis;
+
+
+namespace WinRTSpeechSynthServer;
+
+
+public class Program {
+
+	async static Task Main(string[] args) {
+
+	}
+
+}
