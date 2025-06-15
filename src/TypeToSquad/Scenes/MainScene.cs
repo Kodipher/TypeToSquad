3fda45778ccd326d82168552e99a4ddbcf4fa521
Kodipher <45706822+Kodipher@users.noreply.github.com>
2025-06-15 16:23:24 +0200

    Create debug client communication

--- src/TypeToSquad/Scenes/MainScene.cs
+++ src/TypeToSquad/Scenes/MainScene.cs
@@ -1,5 +1,10 @@
 using Godot;
 using System;
+using System.IO;
+using System.IO.Pipes;
+using System.Reflection.PortableExecutable;
+using System.Threading;
+using System.Threading.Tasks;
 
 
 namespace TypeToSquad.Scenes;
@@ -7,11 +12,34 @@ namespace TypeToSquad.Scenes;
 
 public partial class MainScene : HBoxContainer {
 
+	Button testButton;
+	TextEdit testTextEdit;
+
+	NamedPipeClientStream pipeClientStream;
+	StreamReader reader;
+	StreamWriter writer;
+
 	public override void _Ready() {
+		testButton = GetNode<Button>("Button");
+		testTextEdit = GetNode<TextEdit>("Label");
+		testButton.Pressed += PipeExchange;
+
+		
 	}
 
 
 	public override void _Process(double delta) {
+	}
+
+	public void PipeExchange() {
+		pipeClientStream = new NamedPipeClientStream(".", "TESTPIPE", PipeDirection.InOut, PipeOptions.Asynchronous);
+		reader = new StreamReader(pipeClientStream);
+		writer = new StreamWriter(pipeClientStream);
+		pipeClientStream.Connect(5000);
+		writer.WriteLine(testTextEdit.Text);
+		writer.Flush();
+		testTextEdit.Text = reader.ReadLine();
+		pipeClientStream.Dispose();
 	}
 
 }
