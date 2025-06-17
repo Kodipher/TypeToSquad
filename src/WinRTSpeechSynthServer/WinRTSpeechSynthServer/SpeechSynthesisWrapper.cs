d6bdc18ae688ff53447849d5cae1b18e4169e24b
Kodipher <45706822+Kodipher@users.noreply.github.com>
2025-06-17 19:28:18 +0200

    Add synthesis using Ssml (internal api call only)

--- src/WinRTSpeechSynthServer/WinRTSpeechSynthServer/SpeechSynthesisWrapper.cs
+++ src/WinRTSpeechSynthServer/WinRTSpeechSynthServer/SpeechSynthesisWrapper.cs
@@ -13,28 +13,42 @@ namespace WinRTSpeechSynthServer;
 
 public class SpeechSynthesisWrapper : IDisposable {
 
-	SpeechSynthesizer synth = new();
+	readonly SpeechSynthesizer synth = new();
+
 
 	public async Task<byte[]> SynthesizeTextAsync(string input) {
-
-		// Synthesize
 		using SpeechSynthesisStream stream = await synth.SynthesizeTextToStreamAsync(input);
+		return await GetSpeechStreamContents(stream);
+	}
 
-		// Read as buffer
+	public async Task<byte[]> SynthesizeSsmlAsync(string input) {
+		using SpeechSynthesisStream stream = await synth.SynthesizeSsmlToStreamAsync(input);
+		return await GetSpeechStreamContents(stream);
+	}
+
+	public byte[] SynthesizeText(string input) {
+		return SynthesizeTextAsync(input).GetAwaiter().GetResult();
+	}
+
+	public byte[] SynthesizeSsml(string input) {
+		return SynthesizeSsmlAsync(input).GetAwaiter().GetResult();
+	}
+
+	/// <summary>
+	/// Borrows a <see cref="SpeechSynthesisStream"/>
+	/// to read it and return its data as a byte array.
+	/// </summary>
+	private static async Task<byte[]> GetSpeechStreamContents(SpeechSynthesisStream stream) {
+	
 		if (stream.Size >= int.MaxValue) {
-			throw new ArgumentException($"Input string produced an output of length {stream.Size}, is or longer than int.MaxValue", nameof(input));
+			throw new ArgumentException($"Input string produced an output of length {stream.Size}, which is or longer than int.MaxValue");
 		}
 
 		using var dataReader = new DataReader(stream);
 		uint streamSize = (uint)stream.Size;
 		await dataReader.LoadAsync(streamSize);
 
-		// Return as array
 		return dataReader.ReadBuffer(streamSize).ToArray();
-	}
-
-	public byte[] SynthesizeText(string input) { 
-		return SynthesizeTextAsync(input).GetAwaiter().GetResult();
 	}
 
 
