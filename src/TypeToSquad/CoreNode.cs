1e2f505dd13acdb0ceff7598d60cfc9f9c1f9ae6
Kodipher <45706822+Kodipher@users.noreply.github.com>
2025-06-20 20:04:33 +0200

    Add WindowManager to the main scene

--- src/TypeToSquad/CoreNode.cs
+++ src/TypeToSquad/CoreNode.cs
@@ -16,9 +16,17 @@ public partial class CoreNode : Node {
 	public override void _Ready() {
 		base._Ready();
 
+		// Find Children
+		WindowManager = GetNode<WindowManager>("%WindowManager");
+
+		// Give core
+		WindowManager.RecieveCoreReference(this);
+
 	}
 
 	#region //// Children
+
+	public WindowManager WindowManager { get; private set; } = null!; // Set in _Ready
 
 	#endregion
 
