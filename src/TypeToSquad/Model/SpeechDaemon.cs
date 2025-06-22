028026c4f68cf7f8a7d1a602d48802deb0bcad32
Kodipher <45706822+Kodipher@users.noreply.github.com>
2025-06-22 17:36:02 +0200

    SpeechDaemon: Added launching the daemon

--- src/TypeToSquad/Model/SpeechDaemon.cs
+++ src/TypeToSquad/Model/SpeechDaemon.cs
@@ -27,6 +27,72 @@ public class SpeechDaemon : IDisposable {
 		return Path.Combine(projectRootPath, relativeExecutablePath);
 	}
 
+	Process? daemonProcess = null;
+
+	public void StartDaemon(string pipeName) {
+
+		ObjectDisposedException.ThrowIf(isDisposed, this);
+
+
+		GD.Print($"Starting/restarting the daemon with pipe {pipeName}.");
+
+		// Kill existing
+		daemonProcess?.Kill();
+		daemonProcess?.Dispose();
+		daemonProcess = null;
+
+		// Try start new
+		var daemonStartInfo = new ProcessStartInfo(GetDaemonExecutablePath(), [pipeName]) {
+			UseShellExecute = false,
+			CreateNoWindow = true,
+			RedirectStandardOutput = true,
+			RedirectStandardError = true,
+		};
+
+		daemonProcess = Process.Start(daemonStartInfo);
+		if (daemonProcess is null) {
+			GD.PushError("Daemon could not be started.");
+			return;
+		} else if (daemonProcess.HasExited) {
+			GD.PushError("Daemon processes instantly exited.");
+			return;
+		}
+
+		// Hook output and error
+		static void HandleProcessStandardOutput(object _, DataReceivedEventArgs eventArgs) {
+			if (eventArgs.Data is not null) GD.Print($"[DAEMON] {eventArgs.Data}");
+		}
+
+		static async Task ReadProcessStandardErrorBatched(Process daemon) {
+
+			StringBuilder errStringBuilder = new();
+
+			while (!daemon.HasExited) {
+
+				// Wait for first line
+				string? firstLine = await daemon.StandardError.ReadLineAsync();
+				if (firstLine == null) return; // Stream ended. That means processes terminated.
+
+				errStringBuilder.AppendLine(firstLine);
+
+				// Append every other line
+				while (!daemon.StandardError.EndOfStream) {
+					errStringBuilder.AppendLine(await daemon.StandardError.ReadLineAsync());
+				}
+
+				// Print and clear
+				GD.PushError($"[DAEMON ERROR] {errStringBuilder}");
+				errStringBuilder.Clear();
+			}
+
+		}
+
+		daemonProcess.OutputDataReceived += HandleProcessStandardOutput;
+		daemonProcess.BeginOutputReadLine();
+
+		_ = ReadProcessStandardErrorBatched(daemonProcess);
+	}
+
 	#endregion
 
 	#region //// Disposable
@@ -39,6 +105,8 @@ public class SpeechDaemon : IDisposable {
 
 		if (disposingManaged) {
 			// Dispose managed
+			daemonProcess?.Kill();
+			daemonProcess?.Dispose();
 		}
 
 		// Dispose unmanaged
