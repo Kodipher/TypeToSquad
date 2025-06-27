7ef2c580a0a2ac09a865b9769d4ff0d9fcf9e52c
Kodipher <45706822+Kodipher@users.noreply.github.com>
2025-06-27 23:59:01 +0200

    Add UserSettings record

---
+++ src/TypeToSquad/Model/Settings/UserSettings.cs
@@ -0,0 +1,22 @@
+using Godot;
+using System;
+
+
+namespace TypeToSquad.Model.Settings;
+
+
+public record class UserSettings {
+
+	/// <summary>Name of the current output device.</summary>
+	public readonly FieldOptionsRuntime Device = new();
+
+	/// <summary>Name of the current tts voice.</summary>
+	public readonly FieldOptionsRuntime Voice = new();
+
+	/// <summary>Max number of outputs played at the same time.</summary>
+	public readonly FieldIntRange MaxConcurrentStreams = new(1, 64, defaultValue: 6);
+
+	/// <summary>Number of previous inputs held in memory.</summary>
+	public readonly FieldIntRange HistorySlots = new(0, short.MaxValue, defaultValue: 32);
+
+}
