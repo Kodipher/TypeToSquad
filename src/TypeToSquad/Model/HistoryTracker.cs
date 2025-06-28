9af9a547182770c8decd81c44cc9eb725dcf6f6a
Kodipher <45706822+Kodipher@users.noreply.github.com>
2025-06-28 14:49:24 +0200

    Add HistoryTracker

---
+++ src/TypeToSquad/Model/HistoryTracker.cs
@@ -0,0 +1,116 @@
+using Godot;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace TypeToSquad.Model;
+
+
+public class HistoryTracker {
+
+	/// <summary>History of input, recent input first.</summary>
+	readonly LinkedList<string> history = new();
+
+	/// <summary>
+	/// Returns all stored previous inputs, ordered recent first.
+	/// Does not return the present (currently typed) entry.
+	/// </summary>
+	public string[] GetFullHistory() => history.ToArray();
+
+	/// <summary>Adds an entry to the history</summary>
+	public void AddHistoryEntry(string text, int maxSize) {
+
+		maxSize = Math.Max(maxSize, 0);
+
+		// Reset naviation
+		NavigateReset();
+
+		// Add input
+		history.AddFirst(text);
+		while (history.Count > maxSize) history.RemoveLast();
+	}
+
+
+	#region //// Navigation
+
+	/// <summary>Current navigation node and </summary>
+	LinkedListNode<string>? currentHistoryNode = null;
+
+	/// <summary>
+	/// When navigating history, this is the entry that would be added
+	/// as most recent if history were not being navigated.
+	/// </summary>
+	string? presentEntry = null;
+
+	/// <summary>
+	/// Resets history navigation:
+	/// Places navigation point at present
+	/// and forgets what present is (see <see cref="NavigatePrevious"/>).
+	/// </summary>
+	public void NavigateReset() {
+		currentHistoryNode = null;
+		presentEntry = null;
+	}
+
+	/// <summary>
+	/// Navigates furhter into the past. Returns true if navigation was successful. 
+	/// Remembers the present during the first navigation into the past.
+	/// </summary>
+	public bool TryNavigatePrevious(string currentText, out string queryResult) {
+
+		// No history
+		if (currentHistoryNode == null && history.Count == 0) {
+			queryResult = currentText;
+			return false;
+		}
+
+		// Do not look beyond oldest entry
+		if (currentHistoryNode == history.Last) {
+			queryResult = currentText;
+			return false;
+		}
+
+		// Start of navigation: Store present
+		if (currentHistoryNode == null) {
+			presentEntry = currentText;
+			currentHistoryNode = history.First;
+			queryResult = currentHistoryNode!.Value;
+			return true;
+		}
+
+		// Select next entry
+		currentHistoryNode = currentHistoryNode.Next;
+		queryResult = currentHistoryNode!.Value;
+		return true;
+	}
+
+	/// <summary>
+	/// Navigates further towards present. Returns true if navigation was successful.
+	/// Can return present.
+	/// </summary>
+	public bool TryNavigateNext(string currentText, out string queryResult) {
+
+		// Do not look beyond present
+		if (currentHistoryNode == null) {
+			queryResult = currentText;
+			return false;
+		}
+
+		// Switch from history to present
+		if (currentHistoryNode == history.First) {
+			string present = presentEntry ?? "(null)";
+			NavigateReset();
+			queryResult = present;
+			return true;
+		}
+
+		// Select previous entry
+		currentHistoryNode = currentHistoryNode.Previous;
+		queryResult = currentHistoryNode!.Value;
+		return true;
+	}
+
+	#endregion
+
+}
