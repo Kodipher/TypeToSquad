c249987c64e8d83f439668bf09e5528b45f720c0
Kodipher <45706822+Kodipher@users.noreply.github.com>
2025-07-10 00:42:36 +0200

    Settings: Add saving on exit, remove cancel button

--- src/TypeToSquad/Gui/WindowScenes/SettingsWindow.cs
+++ src/TypeToSquad/Gui/WindowScenes/SettingsWindow.cs
@@ -3,8 +3,7 @@ using System;
 using System.Linq;
 
 using TypeToSquad.Utils;
-using Rephidock.GeneralUtilities.Collections;
-using WinRTSpeechSynthServer.Protocol.Messages;
+using TypeToSquad.Model.Settings;
 
 
 namespace TypeToSquad.Gui.WindowScenes;
@@ -20,12 +19,32 @@ public partial class SettingsWindow : Window, IRefrencesCore {
 
 	#endregion
 
+	#region //// Nodes (except settings)
+
+	// All set in FindNodes, which is called in _Ready
+	BaseButton saveButton = null!;
+
+	void FindNodes() {
+		saveButton = this.GetNodeNotNull<BaseButton>("%SaveButton");
+	}
+
+	#endregion
+
 	public override void _Ready() {
 		this.CloseRequested += OnClose;
+
+		FindNodes();
+		saveButton.Pressed += OnClose;
 	}
 
 	public void OnClose() {
 		GD.Print("Closing settings");
+
+		if (CoreNode is not null) {
+			UserSettingsLoader.Save(CoreNode.UserSettings);
+			CoreNode.AudioManager.SetOutputDeviceFromSettings();
+		}
+
 		this.QueueFree();
 	}
 
