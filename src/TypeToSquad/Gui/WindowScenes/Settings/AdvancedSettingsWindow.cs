a6f8ca60e74e43c142bf28f78118687491914a92
Kodipher <45706822+Kodipher@users.noreply.github.com>
2025-07-11 15:36:07 +0200

    Reorganize advanced settings

--- src/TypeToSquad/Gui/WindowScenes/Settings/AdvancedSettingsWindow.cs
+++ src/TypeToSquad/Gui/WindowScenes/Settings/AdvancedSettingsWindow.cs
@@ -11,14 +11,18 @@ namespace TypeToSquad.Gui.WindowScenes.Settings;
 
 public partial class AdvancedSettingsWindow : SettingsWindow, IRefrencesCore {
 
-
 	protected override void SetupSettingInputs() {
 
 		if (CoreNode is null) return;
 
+		// Voices
 		SetupInputOption(CoreNode.UserSettings.Voice, "%MainVoiceInput");
-		SetupInputOption(CoreNode.UserSettings.Device, "%OutputDeviceInput");
+
+		// Input
 		SetupInputLineEdit(CoreNode.UserSettings.HistorySlots, "%HistorySlotsInput");
+
+		// Audio
+		SetupInputOption(CoreNode.UserSettings.Device, "%OutputDeviceInput");
 		SetupInputLineEdit(CoreNode.UserSettings.MaxConcurrentStreams, "%MaxConcurentInput");
 	}
 
