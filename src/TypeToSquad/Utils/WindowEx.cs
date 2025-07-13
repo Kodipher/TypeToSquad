e3832435c808003e6a60f02d3892308c44ff254e
Kodipher <45706822+Kodipher@users.noreply.github.com>
2025-07-13 14:28:11 +0200

    Allocate main themes

--- src/TypeToSquad/Utils/WindowEx.cs
+++ src/TypeToSquad/Utils/WindowEx.cs
@@ -39,6 +39,27 @@ public partial class WindowEx : Window {
 
 	#endregion
 
+	#region //// Theme swap on keyboard input
+
+	/// <summary>
+	/// The main theme used by window.
+	/// Overrides <see cref="Window.Theme"/>.
+	/// </summary>
+	[Export]
+	public Theme? MainTheme { get; set; } = null;
+
+	/// <summary>
+	/// A theme that patchrs <see cref="MainTheme"/>
+	/// when keyboard input is detected.
+	/// </summary>
+	[Export]
+	public Theme? KeyboardPatchTheme { get; set; } = null;
+
+	void HandleInputEvent() {
+	}
+
+	#endregion
+
 	public override void _Ready() {
 		base._Ready();
 		TryAutoFocusNode();
