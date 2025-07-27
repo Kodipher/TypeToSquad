ef19548ff56cc0f05b91ae195e05128351f11a22
Kodipher <45706822+Kodipher@users.noreply.github.com>
2025-07-27 19:32:36 +0200

    [WIP] Add context parsing

--- src/TypeToSquad/Model/MessageSender.cs
+++ src/TypeToSquad/Model/MessageSender.cs
@@ -1,5 +1,6 @@
 using Godot;
 using System;
+using System.Collections.Generic;
 using System.Text;
 
 using WinRTSpeechSynthServer.Protocol.Messages;
@@ -60,10 +61,127 @@ public partial class MessageSender : IRefrencesCore {
 		);
 	}
 
-	void ParseMessageText(in SynthesizeRequest request, string message) {
-		request.InputString = message;
-		request.IsSsml = false;
+	#region //// Parsing
+
+	record class DepthSegment {
+		public required int Start { get; init; }
+		public required int EndExclusive { get; init; }
+		public required int Depth { get; init; }
+	};
+
+	record class ContextStartSegment : DepthSegment {
+		public required int HintStart { get; init; }
+		public required int HintEndExclusive { get; init; }
+		public required int TextStart { get; init; }
 	}
+
+	/// <summary>
+	/// Returns a list of segments in the message, 
+	/// split by depth of nesting.
+	/// </summary>
+	/// <remarks>
+	/// The first segment is always at depth 0.
+	/// The first segment may have a length of 0.
+	/// The last segment may end at a depth above 0.
+	/// Depth is never negative.
+	/// </remarks>
+	List<DepthSegment> SegmentMessage(string message) {
+
+		// Segment message into depth blocks
+		// Depth blocks include brackets
+		// The first block always has a depth of 0 but may have 0 length
+		List<DepthSegment> depthSegments = new();
+
+		int currentDepth = 0;
+		int currentBlockStartI = 0;
+
+		for (int i = 0; i < message.Length; i++) {
+
+			if (message[i] == '[') {
+				// End block and go deeper
+				depthSegments.Add(new DepthSegment() {
+					Depth = currentDepth,
+					Start = currentBlockStartI,
+					EndExclusive = i
+				});
+
+				currentBlockStartI = i;
+				currentDepth++;
+				continue;
+			}
+
+			if (message[i] == ']' && currentDepth > 0) {
+
+				// End block and go up
+				depthSegments.Add(new DepthSegment() {
+					Depth = currentDepth,
+					Start = currentBlockStartI,
+					EndExclusive = i + 1
+				});
+
+				currentBlockStartI = i + 1;
+				currentDepth--;
+				continue;
+			}
+
+		}
+
+		// Add the last block if not empty
+		if (currentBlockStartI != message.Length) {
+			depthSegments.Add(new DepthSegment() {
+				Depth = currentDepth,
+				Start = currentBlockStartI,
+				EndExclusive = message.Length
+			});
+		}
+
+		// TODO: hints
+
+		// Pad with empty segments to return to depth 0
+		while (depthSegments[^1].Depth != 0) {
+			depthSegments.Add(new DepthSegment() {
+				Depth = depthSegments[^1].Depth - 1,
+				Start = message.Length,
+				EndExclusive = message.Length
+			});
+		}
+		
+		return depthSegments;
+	}
+
+	string SegmentedMessageToSsml(IReadOnlyList<DepthSegment> segments, string message) {
+		return "";
+	}
+
+	void ParseMessageText(in SynthesizeRequest request, string message) {
+
+		var segments = SegmentMessage(message);
+
+		// DEBUG
+		GD.Print("==============");
+		foreach (var item in segments) {
+			GD.Print(message[item.Start..item.EndExclusive], " D=", item.Depth);
+		}
+		GD.Print("==============");
+
+		request.InputString = "";
+		request.IsSsml = false;
+		return;
+
+		// Text-only message
+		if (segments.Count == 1) {
+			request.InputString = message;
+			request.IsSsml = false;
+			return;
+		}
+
+		// Message with contexts
+		request.InputString = SegmentedMessageToSsml(segments, message);
+		request.IsSsml = true;
+	}
+
+	#endregion
+
 
 	public partial class SyntaxHighligher : Godot.SyntaxHighlighter {
 
