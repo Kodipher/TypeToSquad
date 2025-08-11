4222c5ebe6904996b9e2acc6d9d5dca3bdf8eecb
Kodipher <45706822+Kodipher@users.noreply.github.com>
2025-08-11 19:07:46 +0200

    Create FieldStringContextHint

---
+++ src/TypeToSquad/Model/Settings/FieldStringContextHint.cs
@@ -0,0 +1,20 @@
+using System.Linq;
+using Rephidock.GeneralUtilities.Collections;
+
+
+namespace TypeToSquad.Model.Settings;
+
+
+public class FieldStringContextHint : Field<string> {
+
+	public override string ReturnValid(string value) {
+		return value
+				.Trim()
+				.Where(c => !(char.IsWhiteSpace(c) || c == '[' || c == ']'))
+				.ToArray()
+				.JoinString();
+	}
+
+	public FieldStringContextHint(string defaultValue) : base(defaultValue) { }
+
+}
