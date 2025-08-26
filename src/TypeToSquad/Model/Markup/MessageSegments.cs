b83b6c498554f7133cedc2c28a98286d82571b84
Kodipher <45706822+Kodipher@users.noreply.github.com>
2025-08-26 13:24:15 +0200

    MessageSegments: Docs + rename base method

--- src/TypeToSquad/Model/Markup/MessageSegments.cs
+++ src/TypeToSquad/Model/Markup/MessageSegments.cs
@@ -5,11 +5,24 @@ namespace TypeToSquad.Model.Markup;
 
 public abstract record class MessageSegment {
 
+	/// <summary>Start position of this segment in the source string.</summary>
+	/// <remarks>
+	/// When performing text replacements, the source string can be <see cref="Text"/>
+	/// of segment where the replacement took place.
+	/// </remarks>
 	public int Start { get; protected set; } = -1;
+
+	/// <summary>First position beyond this segment in the source string.</summary>
+	/// <remarks>
+	/// When performing text replacements, the source string can be <see cref="Text"/>
+	/// of segment where the replacement took place.
+	/// </remarks>
 	public int EndExclusive { get; protected set; } = -1;
+
+	/// <summary>Inner text of the segment</summary>
 	public string Text { get; protected set; } = "";
 
-	protected static T CreateAsSubstring<T>(int start, int endExclusive, string str) 
+	protected static T CreateBaseAsSubstring<T>(int start, int endExclusive, string str) 
 	where T : MessageSegment, new()
 	{
 		return new T {
@@ -25,7 +38,7 @@ public abstract record class MessageSegment {
 public record class PlainTextSegment : MessageSegment {
 
 	public static PlainTextSegment CreateAsSubstring(int start, int endExclusive, string str) {
-		return MessageSegment.CreateAsSubstring<PlainTextSegment>(start, endExclusive, str);
+		return CreateBaseAsSubstring<PlainTextSegment>(start, endExclusive, str);
 	}
 
 	public static PlainTextSegment CreateFromText(string text) {
@@ -38,7 +51,7 @@ public record class PlainTextSegment : MessageSegment {
 public record class InvalidSegment : MessageSegment {
 
 	public static InvalidSegment CreateAsSubstring(int start, int endExclusive, string str) {
-		return MessageSegment.CreateAsSubstring<InvalidSegment>(start, endExclusive, str);
+		return MessageSegment.CreateBaseAsSubstring<InvalidSegment>(start, endExclusive, str);
 	}
 
 }
@@ -48,10 +61,11 @@ public record class HintSegment : MessageSegment {
 
 	public HintType HintType { get; private set; } = HintType.Unset;
 
+	/// <summary>The processed (trimmed) text of the hint.</summary>
 	public string HintText { get; private set; } = "";
 
 	public static HintSegment CreateAsSubstring(int start, int endExclusive, string str) {
-		var ret = MessageSegment.CreateAsSubstring<HintSegment>(start, endExclusive, str);
+		var ret = MessageSegment.CreateBaseAsSubstring<HintSegment>(start, endExclusive, str);
 		ret.HintText = str[(start + 1)..(endExclusive - 1)].Trim();
 		return ret;
 	}
@@ -65,14 +79,17 @@ public record class HintSegment : MessageSegment {
 
 public record class ContentSegment : MessageSegment {
 
+	/// <summary>The first position that separates hint and payload.</summary>
 	public int HintEndExclusive { get; private set; } = -1;
+
+	/// <summary>The processed (trimmed) text of the hint (content type).</summary>
 	public string HintText { get; private set; } = "";
 	public ContentType ContentType { get; private set; } = ContentType.Invalid;
 
 	public string Payload { get; private set; } = "";
 
 	public static ContentSegment CreateAsSubstring(int start, int hintEndExclusive, int endExclusive, string str) {
-		var ret = MessageSegment.CreateAsSubstring<ContentSegment>(start, endExclusive, str);
+		var ret = MessageSegment.CreateBaseAsSubstring<ContentSegment>(start, endExclusive, str);
 		ret.HintEndExclusive = hintEndExclusive;
 		ret.HintText = str[(start + 1)..hintEndExclusive].Trim().ToLower();
 		ret.Payload = str[(hintEndExclusive + 1)..(endExclusive - 1)];
